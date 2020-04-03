d3e5abad3cea1c9a023f091abfabd583ec463321
Ryan Taylor <rytay123@gmail.com>
2020-04-02 20:05:45 -0400

    Update rename_common.py

--- rename_common.py
+++ rename_common.py
@@ -1,6 +1,6 @@
 import os
 
-rep = input("Enter string to be replaced in filenames")
+rep = input("Enter string to be replaced in filenames: ")
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 
 for f in files:
